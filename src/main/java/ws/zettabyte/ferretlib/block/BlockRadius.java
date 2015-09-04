2a5f7fd8959497542699b9d993ace520e17f4cc8
Gyro <gyrocoder@gmail.com>
2015-09-04 00:42:06 -0400

    ...I forgot the world's most obvious optimization, so here you go.

--- src/main/java/ws/zettabyte/ferretlib/block/BlockRadius.java
+++ src/main/java/ws/zettabyte/ferretlib/block/BlockRadius.java
@@ -3,8 +3,10 @@ package ws.zettabyte.ferretlib.block;
 public class BlockRadius extends BlockRange {
 
 	protected int radius;
+	protected int radiusSq;
 	public BlockRadius(int x, int y, int z, int r) {
 		radius = r;
+		radiusSq = r^2;
 		
 		setBounds(x - r, y - r, z - r, x + r, y + r, z + r);		
 		
@@ -18,10 +20,10 @@ public class BlockRadius extends BlockRange {
 	protected boolean isValidPos(int x, int y, int z) {
 		if (super.isValidPos(x, y, z) == false) return false;
 		//Distance function. TODO: Abstract into a Math package somewhere.
-		int distance = (int)Math.sqrt((double)(((x - ourX)^2)
+		int distanceSq = ((x - ourX)^2)
 				+ ((y - ourY)^2)
-				+ ((z - ourZ)^2)));
-		if(distance > radius) return false;
+				+ ((z - ourZ)^2);
+		if(distanceSq > radiusSq) return false;
 		return true;
 	}
 }
